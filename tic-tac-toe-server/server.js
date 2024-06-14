eafc36e2aa415f4a2a657be14ae1260c887e68ca
aroshishp <ee23btech11009@iith.ac.in>
2024-06-14 20:10:09 +0530

    integrate server

---
+++ tic-tac-toe-server/server.js
@@ -0,0 +1,17 @@
+// import { log } from "console";
+// import { createServer } from "http";
+// import { Server } from "socket.io";
+const { createServer } = require("http");
+const { Server } = require('socket.io');
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: "http://localhost:5173/"
+});
+
+io.on("connection", (socket) => {
+  // console.log(socket)
+  console.log("New user joined socket " + socket.id);
+});
+
+httpServer.listen(3000);
